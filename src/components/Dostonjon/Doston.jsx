54de9565bd25df84dc0735ee351e60b7d2a991bd
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-03 19:32:14 +0500

    sdsd

---
+++ src/components/Dostonjon/Doston.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import NavbarPage from './General/Navbar/NavbarPage'
+
+const ToplamApp = () => {
+    const [doston ,SetDoston]=useState('')
+    
+  return (
+    <>
+    <NavbarPage/>
+    </>
+  )
+}
+
+export default ToplamApp
---
+++ src/components/Dostonjon/General/Navbar/NavbarPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarPage = () => {
+  return (
+    <div>
+      <h1>hello navbar page</h1>
+    </div>
+  )
+}
+
+export default NavbarPage
