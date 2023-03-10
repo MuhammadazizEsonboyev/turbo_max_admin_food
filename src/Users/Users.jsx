35bf0b36bd201f9b006063c667024e04d74a2a88
samandar92 <mavlonovsamandar1992@gmail.com>
2023-03-10 19:08:31 +0500

    sm

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Users() {
   return (
     <div>
-      
+     
     </div>
   )
 }
---
+++ src/components/Samandar/SectionS/SectionS.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SectionS = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default SectionS
