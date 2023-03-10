185c10581a579bf811d7c02a066cfcc7d10b0593
unknown <umirzakovasad@gmail.com>
2023-03-10 19:18:06 +0500

    asda

--- src/components/Asadbek/NavBarSection/NavbarSection.jsx
+++ src/components/Asadbek/NavBarSection/NavbarSection.jsx
@@ -6,10 +6,8 @@ function NavbarSection() {
     <>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-    
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
