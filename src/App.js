de690e93fa56ccddf6ee9151d8b95ed7ce59a0c2
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-03-10 19:09:15 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Asadbek from './components/Asadbek/Asadbek'
+import Muhammadaziz from './components/Muhammadaziz/Muhammadaziz'
+import Users from './Users/Users'
 
 export default function App() {
 
@@ -7,7 +11,13 @@ export default function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Users/>}/>
+            <Route path='/muhammadaziz' element={<Muhammadaziz/>}/>
+            <Route path='/asadbek' element={<Asadbek/>}/>
+          </Routes>
+        </Router>
     </>
   )
 }
--- src/components/Qodirov.jsx
+++ src/components/Qodirov.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Qodirov() {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
