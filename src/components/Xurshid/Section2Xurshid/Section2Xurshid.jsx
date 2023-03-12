0818f1c1ba34d857c193c200c2de90f942f6c6e0
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-03-12 10:37:27 +0500

    xurshid

--- src/components/Xurshid/Section2Xurshid/Section2Xurshid.jsx
+++ src/components/Xurshid/Section2Xurshid/Section2Xurshid.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import "./section2Xurshid.css"
+const imga = require("../img/img1.png")
 
 export default function Section2Xurshid() {
   return (
@@ -10,6 +11,16 @@ export default function Section2Xurshid() {
                 <Col xs={12}>
                 <h1 className='section_2'>Featured works</h1>
                 </Col>
+                <Col xs={4}>
+                    <img src={imga} alt="" />
+                </Col>
+
+                <Col xs={8}>
+                   <h1 className='section2_h1'>Designing Dashboards</h1>
+                   <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. <br />
+                    Velit officia consequat duis enim velit mollit. Exercitation veniam consequat <br /> sunt nostrud amet.</p>
+                </Col>
+
             </Row>
         </Container>
     </div>
