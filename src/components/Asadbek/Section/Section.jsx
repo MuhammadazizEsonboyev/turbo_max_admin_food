5cefcf482f2e151d88b81560019d8dcbd964fa48
unknown <umirzakovasad@gmail.com>
2023-03-14 17:10:57 +0500

    asad

--- src/components/Asadbek/Section/Section.jsx
+++ src/components/Asadbek/Section/Section.jsx
@@ -1,79 +1,124 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import './Section.css'
 
 export default function Section() {
+
   return (
-    <>
-    <Container>
-        <div id='for-responsive'>
-
-        
-        <Row>
+    <> 1
+      <Container>
+        <Row className='justify-content-center'>
+          <div id='for-responsive'>
             <h3 className='works'>Featured works</h3>
-        <Col xs={12}>
-        <div className='df-card'>
-        <div className='img-box'></div>
+            <Col xs={12} lg={12} className='d-block d-sm-none'>
+              <div className='img-box'></div>
+              <div className='df-info'>
+                <h2 className='des-dashboard'>Designing Dashboards</h2>
 
-        <div className='df-info'>
+                <div className='data-Dash'>
+                  <div className='date-box'>
+                    2020
+                  </div>
+                  <h4 className='dashboard'>Dashboard</h4>
+                </div>
+                <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                  nostrud amet.</p>
+              </div>
 
-    
-        <h2 className='des-dash'>Designing Dashboards</h2>
-        <div className='data-Dash'>
-        <div className='date-box'>
-          2020
-        </div>
-        <h4 className='dashboard'>Dashboard</h4>
-        </div>
-        <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
-         nostrud amet.</p>
-        </div>
-        </div>
-        </Col>
-        <hr />
-        <Col xs={12}>
-        <div className='df-card'>
-        <div className='img-box'></div>
+            </Col>
+            <Col xs={12} lg={12} className='d-none d-sm-block'>
+              <div className='df-card'>
+                <div className='img-box'></div>
+                <div className='df-info'>
+                  <h2 className='des-dashboard'>Designing Dashboards</h2>
 
-        <div className='df-info'>
+                  <div className='data-Dash'>
+                    <div className='date-box'>
+                      2020
+                    </div>
+                    <h4 className='dashboard'>Dashboard</h4>
+                  </div>
+                  <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                    nostrud amet.</p>
+                </div>
+              </div>
+            </Col>
+            <hr />
+            <Col xs={12} lg={12} className='d-block d-sm-none'>
+              <div className='img-box'></div>
+              <div className='df-info'>
+                <h2 className='des-dashboard'>Designing Dashboards</h2>
 
-    
-        <h2 className='des-dash'>Vibrant Portraits of 2020</h2>
-        <div className='data-Dash'>
-        <div className='date-box'>
-          2018
-        </div>
-        <h4 className='dashboard'>Illustration</h4>
-        </div>
-        <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
-         nostrud amet.</p>
-        </div>
-        </div>
-        <hr />
-        </Col>
-        <Col xs={12}>
-        <div className='df-card'>
-        <div className='img-box'></div>
+                <div className='data-Dash'>
+                  <div className='date-box'>
+                    2020
+                  </div>
+                  <h4 className='dashboard'>Dashboard</h4>
+                </div>
+                <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                  nostrud amet.</p>
+              </div>
 
-        <div className='df-info'>
+            </Col>
+            <Col xs={12} lg={12} className='d-none d-sm-block'>
+              <div className='df-card'>
+                <div className='img-box'></div>
+                <div className='df-info'>
+                  <h2 className='des-dashboard'>Designing Dashboards</h2>
 
-    
-        <h2 className='des-dash'>36 Days of Malayalam type</h2>
-        <div className='data-Dash'>
-        <div className='date-box'>
-          2018
-        </div>
-        <h4 className='dashboard'>Typography</h4>
-        </div>
-        <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
-         nostrud amet.</p>
-        </div>
-        </div>
-        <hr />
-        </Col>
+                  <div className='data-Dash'>
+                    <div className='date-box'>
+                      2020
+                    </div>
+                    <h4 className='dashboard'>Dashboard</h4>
+                  </div>
+                  <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                    nostrud amet.</p>
+                </div>
+              </div>
+            </Col>
+            <hr />
+
+            <Col xs={12} lg={12} className='d-block d-sm-none'>
+              <div className='img-box'></div>
+              <div className='df-info'>
+                <h2 className='des-dashboard'>Designing Dashboards</h2>
+
+                <div className='data-Dash'>
+                  <div className='date-box'>
+                    2020
+                  </div>
+                  <h4 className='dashboard'>Dashboard</h4>
+                </div>
+                <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                  nostrud amet.</p>
+              </div>
+
+            </Col>
+            <Col xs={12} lg={12} className='d-none d-sm-block'>
+              <div className='df-card'>
+                <div className='img-box'></div>
+                <div className='df-info'>
+                  <h2 className='des-dashboard'>Designing Dashboards</h2>
+
+                  <div className='data-Dash'>
+                    <div className='date-box'>
+                      2020
+                    </div>
+                    <h4 className='dashboard'>Dashboard</h4>
+                  </div>
+                  <p className='mini-info-sec'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit <br /> officia consequat duis enim velit mollit. Exercitation veniam consequat sunt <br />
+                    nostrud amet.</p>
+                </div>
+              </div>
+            </Col>
+            <hr />
+          </div>
         </Row>
-        </div>
-    </Container>
+
+
+
+      </Container>
     </>
   )
 }
